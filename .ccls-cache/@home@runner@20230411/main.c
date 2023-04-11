1125e5e4893a6d06f76adb6b2520a32fcf786a00
30629 <129129762+seoyeon30629@users.noreply.github.com>
2023-04-11 01:11:16 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@20230411/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+main(){
+
+  if (5>3)// if (참)
+    printf("A/n");
+printf("B/b");
+
+if(5+3) printf("C/n");// if (참)
+printf("D/n");
+
+if(5-5) printf("E/n");// if (거짓)
+
+if (a>b) printf("F/n"); // if (참)
+printf("G/n");
+
+if(a<3) printf("H/n"); ''if (거짓)
+printf("I/n");
+}
